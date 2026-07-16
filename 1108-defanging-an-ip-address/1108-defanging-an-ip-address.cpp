b23fef69f31fb127adc425e47125f5d8b1be2a4b
Manvendra-Chauhan-07 <chauhan.manvendra2024@gmail.com>
2026-07-16 11:39:47 +0530

    Time: 0 ms (100%), Space: 7.5 MB (94.33%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        
+        string ans;
+        
+        for(auto ch : address)
+        {
+            if(ch == '.')
+            {
+                ans+= "[.]";
+            }
+            else
+            {
+                ans+=ch;
+            }
+        }
+        return ans;
+    }
+};
