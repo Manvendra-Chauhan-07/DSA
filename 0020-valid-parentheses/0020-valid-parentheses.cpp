ad676e071c254793bb71ac76019c5b6fff767a04
Manvendra-Chauhan-07 <chauhan.manvendra2024@gmail.com>
2026-07-27 20:31:31 +0530

    Time: 0 ms (100%), Space: 8.9 MB (68.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
+        
         int n=s.size();
-        stack<char> s1;
-        for(int i=0;i<n;i++){
-            if(s1.empty()){
-                s1.push(s[i]);
+        stack<char> st;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(st.empty())
+            {
+                st.push(s[i]);
             }
             else
             {
-                if(s1.top()=='('&&s[i]==')'){
-                    s1.pop();
-                }
-                else if(s1.top()=='{'&&s[i]=='}'){
-                    s1.pop();
-                }
-                else if(s1.top()=='['&&s[i]==']'){
-                    s1.pop();
+                if((st.top()=='(' && s[i]==')') || (st.top()=='{' && s[i]=='}') || (st.top()=='[' && s[i]==']'))
+                {
+                    st.pop();
                 }
                 else
                 {
-                    s1.push(s[i]);
+                    st.push(s[i]);
                 }
             }
         }
-        return s1.empty();
+        
+        return st.empty();
     }
 };
